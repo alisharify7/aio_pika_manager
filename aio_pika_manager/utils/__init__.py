2980d1876a5e5425df79daba5735e384c4f482d3
ali sharifi <alisharifyofficial@gmail.com>
2025-04-18 00:23:40 +0330

    💢 move logger to utils module

---
+++ aio_pika_manager/utils/__init__.py
@@ -0,0 +1,12 @@
+"""
+* aiopika connection manager
+* author: github.com/alisharify7
+* email: alisharifyofficial@gmail.com
+* license: see LICENSE for more details.
+* Copyright (c) 2025 - ali sharifi
+* https://github.com/alisharify7/aio_pika_manager
+"""
+
+from .logger import get_async_logger
+
+__all__ = ["get_async_logger"]
